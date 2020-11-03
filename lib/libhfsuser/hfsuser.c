427b7bf169cd641fb45ac43ce98bf84f51afabe0
tab <tab@0x09.net>
2020-11-02 20:44:04 -0500

    Add more verbose logging for read failures

--- lib/libhfsuser/hfsuser.c
+++ lib/libhfsuser/hfsuser.c
@@ -34,6 +34,7 @@
 #include <pthread.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <inttypes.h>
 #include <syslog.h>
 #include <sys/ioctl.h>
 
@@ -554,11 +555,20 @@ static inline int hfs_read_pread(struct hfs_device* dev, void* outbytes, uint64_
 int hfs_read(hfs_volume* vol, void* outbytes, uint64_t length, uint64_t offset, hfs_callback_args* cbargs) {
 	struct hfs_device* dev = vol->cbdata;
 	offset += vol->offset;
+	int ret;
 #ifdef HAVE_UBLIO
-	if(dev->use_ublio)
-		return hfs_read_ublio(dev, outbytes, length, offset);
+	if(dev->use_ublio) {
+		ret = hfs_read_ublio(dev, outbytes, length, offset);
+		goto end;
+	}
 #endif
-	return hfs_read_pread(dev, outbytes, length, offset);
+	ret = hfs_read_pread(dev, outbytes, length, offset);
+
+end:
+	if(ret)
+		hfslib_error("read of %" PRIu64 " bytes at offset %" PRIu64 " failed (block size %" PRIu32 "): %s",
+		             NULL, 0, length, offset, dev->blksize, strerror(-ret));
+	return ret;
 }
 
 
