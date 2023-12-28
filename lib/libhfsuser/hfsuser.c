0x09/hfsfuse

commit 041df06cd8285cc7a0d15db725a24a8b20266b20

Translate IFMT flags to system native values
These higher bits of st_mode are not standardized and so the value of
e.g. S_IFDIR may differ between the filesystem and the host system.

TN1150 does define specific values for these flags, so we use the
provided definitions to convert to the system's corresponding S_IF*
values.

lib/libhfsuser/hfsuser.c
@@ -349,13 +349,37 @@ end:
 
 #define HFSTIMETOSPEC(x) ((struct timespec){ .tv_sec = HFSTIMETOEPOCH(x) })
 
+// POSIX 08 specifies values for all file modes below 07777 but leaves the following to the implementation
+// so for these we translate to the system's modes from the definitions given in TN1150
+#define HFS_S_IFMT 0170000
+
+#ifndef S_IFLNK
+#define S_IFLNK 0
+#endif
+#ifndef S_IFSOCK
+#define S_IFSOCK 0
+#endif
+#ifndef S_IFWHT // BSD specific
+#define S_IFWHT 0
+#endif
+
+#define HFS_IFMODES\
+	X(S_IFIFO, 0010000)\
+	X(S_IFCHR, 0020000)\
+	X(S_IFDIR, 0040000)\
+	X(S_IFBLK, 0060000)\
+	X(S_IFREG, 0100000)\
+	X(S_IFLNK, 0120000)\
+	X(S_IFSOCK,0140000)\
+	X(S_IFWHT, 0160000)
+
 void hfs_stat(hfs_volume* vol, hfs_catalog_keyed_record_t* key, struct stat* st, uint8_t fork) {
 	st->st_ino = key->file.cnid;
 
 	struct hfs_device* dev = vol->cbdata;
 
 	// per TN1150, in this case the mode, user, and group are treated as uninitialized and should use defaults
-	if ((key->file.bsd.file_mode & S_IFMT) == 0) {
+	if(!(key->file.bsd.file_mode & HFS_S_IFMT)) {
 		if(key->type == HFS_REC_FILE) {
 			st->st_mode = dev->default_file_mode | S_IFREG;
 		} else {
@@ -364,7 +388,11 @@ void hfs_stat(hfs_volume* vol, hfs_catalog_keyed_record_t* key, struct stat* st,
 		st->st_uid = dev->default_uid;
 		st->st_gid = dev->default_gid;
 	} else {
-		st->st_mode = key->file.bsd.file_mode;
+		st->st_mode  = key->file.bsd.file_mode & 0xFFF;
+
+		#define X(mode,mask) if(key->file.bsd.file_mode & mask) st->st_mode |= mode;
+		HFS_IFMODES
+		#undef X
 
 		if(key->file.bsd.owner_id > UID_MAX) {
 			hfslib_error("hfs_stat: owner_id %" PRIu32 " too large for CNID %" PRIu32 ", using default",NULL,0,key->file.bsd.owner_id,key->file.cnid);

commit 8a1a78c082672054040d1961641812a41b91865e

Fix translation between HFS+ and POSIX modes in hfs_stat
Fixes a bug introduced in 041df06cd8285cc7a0d15db725a24a8b20266b20
that causes directories to be interpreted as whiteouts.

lib/libhfsuser/hfsuser.c
@@ -390,7 +390,7 @@ void hfs_stat(hfs_volume* vol, hfs_catalog_keyed_record_t* key, struct stat* st,
 	} else {
 		st->st_mode  = key->file.bsd.file_mode & 0xFFF;
 
-		#define X(mode,mask) if(key->file.bsd.file_mode & mask) st->st_mode |= mode;
+		#define X(mode,mask) if((key->file.bsd.file_mode & mask) == mask) st->st_mode |= mode;
 		HFS_IFMODES
 		#undef X
 