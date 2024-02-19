a5ceefac67f0d0ad07d6a6e1fcef8be43a16d9dc
tab <tab@0x09.net>
2024-02-19 16:51:08 -0500

    Squashed 'lib/utf8proc/' changes from 462093b..34db3f7
    34db3f7 untar into new directory
    e9d2557 make distcheck should keep tarball, rm directory
    8bf9d71 add make distcheck
    bb84f2e make dist target
    05886a9 version 2.9 bump (#254)
    46a442b Unicode 15.1 support (#253)
    1cb28a6 v2.8.0 bump (#248)
    3c49294 unicode 15 support (#247)
    1f1e42d Add c flag when invoking ar (#241)
    63f31c9 Improve fuzzer code coverage (#239)
    39dbf50 fuzz: limit input length (#238)
    f0b3707 don't use make in cmake instructions (closes #236)
    2484e2e update Doxygen config with doxygen -u
    8ca6144 copyright year update
    e3b9a89 prepare for 2.7.0 release
    b093cf9 update for unicode 14 (#233)
    0e59e0b rm travis
    7c14eda update gitignore
    bab7aec [ci] set github CI (#229)
    27e8a16 cmake: fix installation directories and also install pkgconfig file (#224)
    8a4cd4c reduce lenencode bits (#232)
    
    git-subtree-dir: lib/utf8proc
    git-subtree-split: 34db3f7954e9298e89f42641ac78e0450f80a70d

--- test/fuzzer.c
+++ test/fuzzer.c
@@ -5,10 +5,13 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     if(size < 1) return 0;
 
+    /* Avoid timeout with long inputs */
+    if(size > (64 * 1024)) return 0;
+
     if(data[size-1] != '\0') return 0;
 
     const uint8_t* ptr = data;
-    utf8proc_int32_t c = 0;
+    utf8proc_int32_t c = 0, c_prev = 0, state = 0;
     utf8proc_option_t options;
     utf8proc_ssize_t ret, bytes = 0;
     size_t len = strlen((const char*)data);
@@ -31,6 +34,11 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
         utf8proc_category(c);
         utf8proc_category_string(c);
         utf8proc_codepoint_valid(c);
+        
+        utf8proc_grapheme_break(c_prev, c);
+        utf8proc_grapheme_break_stateful(c_prev, c, &state);
+        
+        c_prev = c;
     }
     
     utf8proc_int32_t *copy = size >= 4 ? NULL : malloc(size);
@@ -54,7 +62,11 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
         options = UTF8PROC_STRIPCC;
         memcpy(copy, data, size);
         utf8proc_normalize_utf32(copy, size, options);
-        
+
+        options = UTF8PROC_LUMP;
+        memcpy(copy, data, size);
+        utf8proc_normalize_utf32(copy, size, options);
+
         options = 0;
         memcpy(copy, data, size);
         utf8proc_normalize_utf32(copy, size, options);
