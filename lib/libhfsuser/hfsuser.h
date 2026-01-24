e02deb9d558f9063bcfcdabbda5a356fa8c99bc2
tab <tab@0x09.net>
2026-01-24 18:12:06 -0500

    hfsuser: fix support for very old timestamps
    Previously hfsfuse and tools clamped times before the Unix epoch to
    1970, but HFS+ is perfectly capable of storing timestamps prior to this
    and as old as 1903.

--- lib/libhfsuser/hfsuser.h
+++ lib/libhfsuser/hfsuser.h
@@ -11,7 +11,7 @@
 
 #include "libhfs.h"
 
-#define HFSTIMETOEPOCH(x) (x>2082844800?x-2082844800:0)
+#define HFSTIMETOEPOCH(x) ((x)-2082844800L)
 
 // maximum bytes an HFS+ path element can occupy in UTF-8
 #define HFS_NAME_MAX 765
