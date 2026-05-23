c4e1dde1e67a4787da162bb3397fd2216ff2d182
tab <tab@0x09.net>
2026-05-23 16:07:33 -0400

    hfsfuse: invert check for crtime vs btime added in 4ac0f5819002a4db45bb939f1b314ef28fc4a78f

--- src/hfsfuse.c
+++ src/hfsfuse.c
@@ -127,7 +127,7 @@ static int hfsfuse_readlink(const char* path, char* buf, size_t size) {
 }
 
 #if FUSE_DARWIN_ENABLE_EXTENSIONS
-#if HAVE_DARWIN_ATTR_BTIME
+#if HAVE_DARWIN_ATTR_CRTIME
 #define stat_to_fuse_darwin_attr(rec,st) ((struct fuse_darwin_attr){\
 	.ino = (st).st_ino,\
 	.mode = (st).st_mode,\
@@ -138,7 +138,7 @@ static int hfsfuse_readlink(const char* path, char* buf, size_t size) {
 	.atimespec.tv_sec = (st).st_atime,\
 	.mtimespec.tv_sec = (st).st_mtime,\
 	.ctimespec.tv_sec = (st).st_ctime,\
-	.btimespec.tv_sec = (st).st_birthtime,\
+	.crtimespec.tv_sec = (st).st_birthtime,\
 	.bkuptimespec.tv_sec = HFSTIMETOEPOCH((rec).file.date_backedup),\
 	.size = (st).st_size,\
 	.blocks = (st).st_blocks,\
@@ -156,7 +156,7 @@ static int hfsfuse_readlink(const char* path, char* buf, size_t size) {
 	.atimespec.tv_sec = (st).st_atime,\
 	.mtimespec.tv_sec = (st).st_mtime,\
 	.ctimespec.tv_sec = (st).st_ctime,\
-	.crtimespec.tv_sec = (st).st_birthtime,\
+	.btimespec.tv_sec = (st).st_birthtime,\
 	.bkuptimespec.tv_sec = HFSTIMETOEPOCH((rec).file.date_backedup),\
 	.size = (st).st_size,\
 	.blocks = (st).st_blocks,\
